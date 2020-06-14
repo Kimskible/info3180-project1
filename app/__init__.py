d6b514c85c449afe9cd5973f39da96ae10f37821
Kimberly Stewart <47463057+Kimskible@users.noreply.github.com>
2020-06-14 01:41:03 -0500

    removed key

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,9 @@ from os.path import join, dirname, realpath
 
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = "h@jkLdw84nj@!"
+# app.config['SECRET_KEY'] = "secretkey"
 app.config['SECRET_KEY'] = secrets.token_urlsafe(32)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://pro1:password@localhost/pro1"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://username:password@localhost/dbname"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 # File upload config
 UPLOAD_FOLDER = join(dirname(realpath(__file__)), 'static/uploads')
